0425b3290f9fb1c34f52e0e1a81b2fb2bf6f4ca8
nikachu404 <yasinskaa.veronika@gmail.com>
2023-07-01 15:50:16 +0300

    created constants file

---
+++ src/constants.ts
@@ -0,0 +1,7 @@
+export const platformOptions = [
+  { value: 'Twitch', label: 'Twitch', icon: 'fab fa-twitch' },
+  { value: 'YouTube', label: 'YouTube', icon: 'fab fa-youtube' },
+  { value: 'TikTok', label: 'TikTok', icon: 'fab fa-tiktok' },
+  { value: 'Kick', label: 'Kick', icon: 'fab fa-kickstarter' },
+  { value: 'Rumble', label: 'Rumble', icon: 'fa-solid fa-play' },
+];
