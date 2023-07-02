304adfb49b241013adf961cc86aa8d00415e5f6e
nikachu404 <yasinskaa.veronika@gmail.com>
2023-07-02 20:34:18 +0300

    added FormData type

---
+++ src/types/FormData.ts
@@ -0,0 +1,8 @@
+export type FormData = {
+  name: string;
+  platform: string;
+  description: string;
+  avatarPreview: string | null;
+  avatarFile: File | null;
+  isVisibleOnMobile: boolean;
+};
