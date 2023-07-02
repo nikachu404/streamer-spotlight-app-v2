2f2a475f3dd86fe66046a9c132cb0435d5a50a82
nikachu404 <yasinskaa.veronika@gmail.com>
2023-07-02 14:41:54 +0300

    added filter

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,23 +3,27 @@ import {
   StreamerSubmissionForm,
   StreamerList,
   Pagination,
+  Filter,
 } from '../../components';
 import { Streamer } from '../../types/Streamer';
 import { getStreamers } from '../../api/streamers';
+import { streamersPerPage } from '../../constants';
 import './home.scss';
 
 export const Home: React.FC = () => {
   const [streamers, setStreamers] = useState<Streamer[]>([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [totalPages, setTotalPages] = useState(1);
+  const [sortBy, setSortBy] = useState('');
+  const [sortOrder, setSortOrder] = useState('');
 
   useEffect(() => {
     fetchStreamers();
-  }, [currentPage]);
+  }, [currentPage, sortBy, sortOrder]);
 
   const fetchStreamers = async () => {
     try {
-      const response = await getStreamers(currentPage, 4);
+      const response = await getStreamers(currentPage, streamersPerPage, sortBy, sortOrder);
       setStreamers(response.streamers);
       setTotalPages(response.totalPages);
     } catch (error) {
@@ -36,9 +40,17 @@ export const Home: React.FC = () => {
     setCurrentPage(page);
   };
 
+  const handleFilterChange = (sortBy: string, sortOrder: string) => {
+    setSortBy(sortBy);
+    setSortOrder(sortOrder);
+  };
+
   return (
     <div className="home">
       <div className="home__main">
+        <div className="home__filter">
+          <Filter onFilterChange={handleFilterChange} />
+        </div>
         <StreamerList streamers={streamers} fetchStreamers={fetchStreamers} />
         <Pagination
           totalPages={totalPages}
