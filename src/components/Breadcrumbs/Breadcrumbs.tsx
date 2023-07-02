95ab4f1656f8aaae7e8bf4ce1eca724aec85768c
nikachu404 <yasinskaa.veronika@gmail.com>
2023-07-02 14:38:00 +0300

    created breadcrumbs component

---
+++ src/components/Breadcrumbs/Breadcrumbs.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import './breadcrumbs.scss';
+
+type Props = {
+  streamerName: string;
+};
+
+export const Breadcrumbs: React.FC<Props> = ({ streamerName }) => {
+  const navigate = useNavigate();
+
+  const goBack = () => {
+    navigate(-1);
+  };
+
+  return (
+    <div className="breadcrumbs">
+      <span className="breadcrumbs__back" onClick={goBack}>
+        Back
+      </span>
+      <i className="fa-solid fa-angle-right" />
+      <span>{streamerName}</span>
+    </div>
+  );
+};
